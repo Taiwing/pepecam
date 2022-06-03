d4e0d73b1da83a0bec64f53fa78e7aaf6753a2b0
Yoann FOREAU <yforeau@e2r12p9.clusters.42paris.fr>
2022-06-03 11:35:40 +0200

    feat: add postgres initiliaztion

---
+++ db/create_tables.sql
@@ -0,0 +1,36 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS accounts (
+	account_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	email VARCHAR(256) NOT NULL,
+	username VARCHAR(64) NOT NULL,
+	password_hash VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pictures (
+	picture_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	account_id UUID NOT NULL,
+	creation_ts DATE NOT NULL DEFAULT CURRENT_DATE,
+	picture_path VARCHAR(256) NOT NULL
+);
+
+ALTER TABLE pictures ADD FOREIGN KEY (account_id) REFERENCES accounts (account_id);
+
+CREATE TABLE IF NOT EXISTS likes (
+	picture_id UUID NOT NULL,
+	account_id UUID NOT NULL,
+	CONSTRAINT NO_DUPLICATE_LIKE UNIQUE (picture_id, account_id)
+);
+
+ALTER TABLE likes ADD FOREIGN KEY (picture_id) REFERENCES pictures (picture_id);
+ALTER TABLE likes ADD FOREIGN KEY (account_id) REFERENCES accounts (account_id);
+
+CREATE TABLE IF NOT EXISTS comments (
+	picture_id UUID NOT NULL,
+	account_id UUID NOT NULL,
+	creation_ts DATE NOT NULL DEFAULT CURRENT_DATE,
+	content VARCHAR(512) NOT NULL
+);
+
+ALTER TABLE comments ADD FOREIGN KEY (picture_id) REFERENCES pictures (picture_id);
+ALTER TABLE comments ADD FOREIGN KEY (account_id) REFERENCES accounts (account_id);

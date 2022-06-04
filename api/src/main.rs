1fa15c2b4f3e39fd4ae9105144247b70f32f6f05
Yoann FOREAU <yforeau@e1r12p6.42.fr>
2022-06-04 18:58:11 +0200

    feat: create Session Guard to check if loggedin

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,13 +1,54 @@
 #[macro_use] extern crate rocket;
 
+use rocket::http::Status;
+use rocket::request::{Outcome, Request, FromRequest};
+
+struct Session {
+	account_id: String,
+}
+
+#[derive(Debug)]
+enum SessionError {
+	LoggedIn,
+	NotLoggedIn,
+	InvalidSession,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Session {
+	type Error = SessionError;
+
+	async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+		fn is_valid(account_id: &str) -> bool {
+			account_id == "valid_account_id" //TODO: check in db
+		}
+
+		match request.cookies().get_private("account_id") {
+			None => Outcome::Failure(
+				(Status::BadRequest, SessionError::NotLoggedIn)
+			),
+			Some(cookie) if is_valid(cookie.value()) => Outcome::Success(
+				Session { account_id: cookie.value().to_string() }
+			),
+			Some(_) => Outcome::Failure(
+				(Status::BadRequest, SessionError::InvalidSession)
+			),
+		}
+	}
+}
+
 #[post("/register")]
 fn register() -> &'static str {
 	"register\n"
+	//TODO: generate confirmation_token and send it through an email
 }
 
 #[post("/confirm/<confirmation_token>")]
 fn confirm(confirmation_token: u128) -> String {
 	format!("confirm your email with: {}\n", confirmation_token)
+	//TODO: check confirmation token
+	//add new user to database and log user if the token is valid
+	//return an error otherwise
 }
 
 #[put("/login")]
@@ -25,14 +66,15 @@ fn reset_token() -> &'static str {
 	"reset-token: request reset token\n"
 }
 
-#[put("/password")]
-fn password() -> &'static str {
-	"password: use reset-token to reset password\n"
+#[put("/password", data = "<reset_token>")]
+fn password(reset_token: &str) -> String {
+	format!("password: use reset-token '{}' to reset password\n", reset_token)
 }
 
 #[put("/")]
-fn put_user() -> &'static str {
-	"PUT user/: change username, password and/or email settings\n"
+fn put_user(session: Session) -> String {
+	format!("PUT user ({}): change username, password and/or email settings\n",
+		session.account_id)
 }
 
 #[get("/")]
@@ -46,12 +88,12 @@ fn get_user_pictures(username: &str) -> String {
 }
 
 #[put("/like/<picture_id>")]
-fn like(picture_id: &str) -> String {
+fn like(picture_id: &str, session: Session) -> String {
 	format!("PUT toggle like on picture {}\n", picture_id)
 }
 
 #[put("/comment/<picture_id>", data = "<content>")]
-fn comment(picture_id: &str, content: &str) -> String {
+fn comment(picture_id: &str, content: &str, session: Session) -> String {
 	format!("PUT comment '{}' on picture {}\n", content, picture_id)
 }
 

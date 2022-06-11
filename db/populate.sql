7dedc4f75d2af51d5bdb0ecd1c72d2316406ec1d
Yoann FOREAU <yforeau@e1r12p5.42.fr>
2022-06-11 17:08:05 +0200

    feat: finish populate script

--- db/populate.sql
+++ db/populate.sql
@@ -1,10 +1,8 @@
 -- Set params
 SET SESSION test.n_accounts = '50';
 SET SESSION test.n_pictures = '500';
-SET SESSION test.n_likes_min = '0';
-SET SESSION test.n_likes_max = '50';
-SET SESSION test.n_comments_min = '0';
-SET SESSION test.n_comments_max = '100';
+SET SESSION test.n_likes = '1000';
+SET SESSION test.n_comments = '1000';
 SET SESSION test.date_min = '2022-01-01 00:00:00';
 SET SESSION test.date_max = '2022-06-11 00:00:00';
 SET SESSION test.password_hash = '1e623de0d44a6e7443ab927559c02452';
@@ -48,6 +46,19 @@ $$
 	END
 $$;
 
+-- Random Picture function
+CREATE FUNCTION random_picture()
+	RETURNS UUID
+	LANGUAGE PLPGSQL
+AS
+$$
+	DECLARE picture UUID;
+	BEGIN
+		SELECT picture_id INTO picture FROM pictures ORDER BY RANDOM() LIMIT 1;
+		RETURN picture;
+	END
+$$;
+
 -- Generate Pictures
 INSERT INTO pictures
 SELECT
@@ -55,3 +66,21 @@ SELECT
 	random_user(),
 	random_date(CURRENT_SETTING('test.date_min'), CURRENT_SETTING('test.date_max'))
 FROM GENERATE_SERIES(1, CURRENT_SETTING('test.n_pictures')::int);
+
+-- Generate Likes
+INSERT INTO likes
+SELECT
+	random_picture(),
+	random_user()
+FROM GENERATE_SERIES(1, CURRENT_SETTING('test.n_likes')::int)
+ON CONFLICT DO NOTHING;
+
+-- Generate Comments
+INSERT INTO comments
+SELECT
+	random_picture(),
+	random_user(),
+	random_date(CURRENT_SETTING('test.date_min'), CURRENT_SETTING('test.date_max')),
+	md5(random()::TEXT)
+FROM GENERATE_SERIES(1, CURRENT_SETTING('test.n_comments')::int)
+ON CONFLICT DO NOTHING;

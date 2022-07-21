370e7f95265e74a09e5f04733c6fc95413478773
yoann foreau <yoann.foreau@ito.am>
2022-07-21 21:37:22 +0200

    docs: add things to do for the login/logout

--- api/src/routes/user/logout.rs
+++ api/src/routes/user/logout.rs
@@ -3,8 +3,12 @@ use crate::payload::DefaultResponse;
 use crate::result::ApiResult;
 use rocket::http::{Cookie, CookieJar, Status};
 
-//TODO: fix this (remove_private is not enough because if the cookie has been
-//kept on the user side it still can be used for authentication)
+//TODO: Fix this (remove_private is not enough because if the cookie has been
+// kept on the user side it still can be used for authentication). Change the
+// session cookie to add a cookie unique identifier so that it can be retrieved
+// in a session cache. Also add an expiry field to it (which will be the same as
+// the cookie itself), so that it can be removed from the cache when it is
+// expired.
 
 /// Log out of the application.
 #[put("/logout")]

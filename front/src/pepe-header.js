73bc78dd3afa69884ba9d14b914677a3cb9dd67b
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-04-26 11:04:45 +0200

    feat: successfully send login and signup forms

--- front/src/pepe-header.js
+++ front/src/pepe-header.js
@@ -1,4 +1,4 @@
-const formField = (name, type, label, placeholder, optional) => {
+const formField = (name, type, label, placeholder) => {
   const labelElement = document.createElement('label')
   labelElement.setAttribute('for', name)
   labelElement.textContent = label || name
@@ -7,16 +7,34 @@ const formField = (name, type, label, placeholder, optional) => {
   input.setAttribute('type', type)
   input.setAttribute('name', name)
   input.setAttribute('placeholder', placeholder || name)
-  if (!optional) {
-    input.setAttribute('required', '')
-  }
+  input.setAttribute('required', '')
   labelElement.append(input)
   return labelElement
 }
 
 const capitalize = (str) => str[0].toUpperCase() + str.slice(1)
 
-const buildFormDialog = (formName, formFields) => {
+const postForm = (form, action) => {
+  const formData = new FormData(form)
+
+  const data = {}
+  for (const [key, value] of formData.entries()) {
+    if (key !== 'password-confirm') {
+      data[key] = value
+    }
+  }
+
+  const headers = new Headers()
+  headers.append('Content-Type', 'application/json')
+
+  return fetch(action, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(data),
+  })
+}
+
+const buildFormDialog = (formName, action, formFields) => {
     const dialog = document.createElement('dialog')
     dialog.setAttribute('id', `${formName}-dialog`)
 
@@ -30,8 +48,8 @@ const buildFormDialog = (formName, formFields) => {
 
     const forms = []
     for (const field of formFields) {
-      const { name, type, label, placeholder, optional } = field
-      const fieldElement = formField(name, type, label, placeholder, optional)
+      const { name, type, label, placeholder } = field
+      const fieldElement = formField(name, type, label, placeholder)
       fieldElement.setAttribute('id', `${formName}-${name}`)
       forms.push(fieldElement)
     }
@@ -41,6 +59,23 @@ const buildFormDialog = (formName, formFields) => {
     const submit = document.createElement('button')
     submit.setAttribute('type', 'submit')
     submit.textContent = formName
+    submit.addEventListener('click', async () => {
+      try {
+        if (form.reportValidity() === false) return
+        const response = await postForm(form, action)
+        if (response.ok) {
+          const message = await response.json()
+          alert(`Success: ${JSON.stringify(message)}`) //TEMP
+        } else {
+          const { message, error } = response.json()
+          const errorMessage = message || error || JSON.stringify(response)
+          alert(`Error: ${errorMessage}`)
+        }
+      } catch (error) {
+        console.log('toto')
+        alert(`Error: ${error}`)
+      }
+    })
     const cancel = document.createElement('button')
     cancel.setAttribute('type', 'reset')
     cancel.textContent = 'cancel'
@@ -59,7 +94,7 @@ const loginFields = [
 
 const signupFields = [
   { name: 'username', type: 'text' },
-  { name: 'email', type: 'email', optional: true },
+  { name: 'email', type: 'email' },
   { name: 'password', type: 'password' },
   {
     name: 'password-confirm',
@@ -80,8 +115,16 @@ class PepeHeader extends HTMLElement {
     const home = document.createElement('a')
     home.href = '/'
 
-    const loginDialog = buildFormDialog('login', loginFields)
-    const signupDialog = buildFormDialog('signup', signupFields)
+    const loginDialog = buildFormDialog(
+      'login',
+      'http://localhost:3000/user/login',
+      loginFields,
+    )
+    const signupDialog = buildFormDialog(
+      'signup',
+      'http://localhost:3000/user/register',
+      signupFields,
+    )
     const passwordField = signupDialog
       .querySelector('#signup-password')
       .querySelector('input')

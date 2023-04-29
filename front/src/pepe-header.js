8d087666740273f3440fd02497a329dc7a0b2ad5
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-04-29 16:24:16 +0200

    refactor: use template for PepeGallery

--- front/src/pepe-header.js
+++ front/src/pepe-header.js
@@ -1,22 +1,6 @@
 import { getCookie, toggleConnectedEvent } from './utils.js'
 
-const formField = (name, type, label, placeholder) => {
-  const labelElement = document.createElement('label')
-  labelElement.setAttribute('for', name)
-  labelElement.textContent = label || name
-  labelElement.classList.add('form-field')
-  const input = document.createElement('input')
-  input.setAttribute('type', type)
-  input.setAttribute('name', name)
-  input.setAttribute('placeholder', placeholder || name)
-  input.setAttribute('required', '')
-  labelElement.append(input)
-  return labelElement
-}
-
-const capitalize = (str) => str[0].toUpperCase() + str.slice(1)
-
-const postForm = (form, action) => {
+const postForm = (form, url) => {
   const formData = new FormData(form)
 
   const data = {}
@@ -29,7 +13,7 @@ const postForm = (form, action) => {
   const headers = new Headers()
   headers.append('Content-Type', 'application/json')
 
-  return fetch(action, {
+  return fetch(url, {
     method: 'POST',
     headers,
     credentials: 'include',
@@ -37,121 +21,101 @@ const postForm = (form, action) => {
   })
 }
 
-const buildFormDialog = (formName, action, formFields) => {
-    const dialog = document.createElement('dialog')
-    dialog.setAttribute('id', `${formName}-dialog`)
+async function dialogSubmit() {
+  const url = this.getAttribute('url')
+  const form = this.querySelector('form')
+  const submit = this.querySelector('button[type="submit"]')
 
-    const title = document.createElement('h3')
-    title.textContent = capitalize(formName)
-
-    const form = document.createElement('form')
-    form.setAttribute('id', `${formName}-form`)
-    form.setAttribute('method', 'dialog')
-    form.classList.add('form')
-
-    const forms = []
-    for (const field of formFields) {
-      const { name, type, label, placeholder } = field
-      const fieldElement = formField(name, type, label, placeholder)
-      fieldElement.setAttribute('id', `${formName}-${name}`)
-      forms.push(fieldElement)
+  try {
+    if (form.reportValidity() === false) return
+    const response = await postForm(form, url)
+    if (response.ok) {
+      const message = await response.json()
+      alert(`Success: ${JSON.stringify(message)}`) //TEMP
+      submit.dispatchEvent(toggleConnectedEvent())
+    } else {
+      const { message, error } = await response.json()
+      const errorMessage = message || error || JSON.stringify(response)
+      alert(`Error: ${errorMessage}`)
     }
-
-    const buttons = document.createElement('div')
-    buttons.classList.add('form-field')
-    const submit = document.createElement('button')
-    submit.setAttribute('type', 'submit')
-    submit.textContent = formName
-    submit.addEventListener('click', async () => {
-      try {
-        if (form.reportValidity() === false) return
-        const response = await postForm(form, action)
-        if (response.ok) {
-          const message = await response.json()
-          alert(`Success: ${JSON.stringify(message)}`) //TEMP
-          submit.dispatchEvent(toggleConnectedEvent())
-        } else {
-          const { message, error } = await response.json()
-          const errorMessage = message || error || JSON.stringify(response)
-          alert(`Error: ${errorMessage}`)
-        }
-      } catch (error) {
-        alert(`Error: ${error}`)
-      }
-    })
-    const cancel = document.createElement('button')
-    cancel.setAttribute('type', 'reset')
-    cancel.textContent = 'cancel'
-    cancel.addEventListener('click', () => dialog.close())
-    buttons.append(submit, cancel)
-
-    form.append(...forms, buttons)
-    dialog.append(title, form)
-    return dialog
+  } catch (error) {
+    alert(`Error: ${error}`)
+  }
 }
 
-const loginFields = [
-  { name: 'username', type: 'text' },
-  { name: 'password', type: 'password' },
-]
+const PepeHeaderTemplate = document.createElement('template')
+PepeHeaderTemplate.innerHTML = `
+  <style>@import "style/pepe-header.css"</style>
+  <a href="/"></a>
+  <div id="unconnected" class="user-actions">
+    <button id="login">login</button>
+    <button id="signup">signup</button>
+  </div>
+  <div id="connected" class="user-actions">
+    <button id="logout">logout</button>
+  </div>
 
-const signupFields = [
-  { name: 'username', type: 'text' },
-  { name: 'email', type: 'email' },
-  { name: 'password', type: 'password' },
-  {
-    name: 'password-confirm',
-    type: 'password',
-    label: 'confirm',
-    placeholder: 'password',
-  },
-]
+  <dialog id="login-dialog" url="http://localhost:3000/user/login">
+    <h3>Login</h3>
+    <form id="login-form" method="dialog" class="form">
+      <label for="username" class="form-field">
+        username
+        <input type="text" name="username" placeholder="username" required>
+      </label>
+      <label for="password" class="form-field">
+        password
+        <input type="password" name="password" placeholder="password" required>
+      </label>
+      <div class="form-field">
+        <button type="submit">login</button>
+        <button type="reset">cancel</button>
+      </div>
+    </form>
+  </dialog>
+
+  <dialog id="signup-dialog" url="http://localhost:3000/user/register">
+    <h3>Signup</h3>
+    <form id="signup-form" method="dialog" class="form">
+      <label for="username" class="form-field">
+        username
+        <input type="text" name="username" placeholder="username" required>
+      </label>
+      <label for="email" class="form-field">
+        email
+        <input type="email" name="email" placeholder="email" required>
+      </label>
+      <label for="password" class="form-field">
+        password
+        <input type="password" name="password" placeholder="password" required>
+      </label>
+      <label for="password-confirm" class="form-field">
+        confirm
+        <input
+          type="password"
+          name="password-confirm"
+          placeholder="password"
+          required
+        >
+      </label>
+      <div class="form-field">
+        <button type="submit">signup</button>
+        <button type="reset">cancel</button>
+      </div>
+    </form>
+  </dialog>
+`
 
 // PepeHeader element
 class PepeHeader extends HTMLElement {
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(PepeHeaderTemplate.content.cloneNode(true))
 
-    const style = document.createElement('style')
-    style.textContent = `@import "style/pepe-header.css"`
-    const home = document.createElement('a')
-    home.href = '/'
-
-    const unconnectedElement = document.createElement('div')
-    unconnectedElement.setAttribute('id', 'unconnected')
-    unconnectedElement.classList.add('user-actions')
-
-    const connectedElement = document.createElement('div')
-    connectedElement.setAttribute('id', 'connected')
-    connectedElement.classList.add('user-actions')
-
-    const toggleConnected = ({ detail: { connected } }) => {
-      if (connected) {
-        unconnectedElement.style.display = 'none'
-        connectedElement.style.display = 'flex'
-      } else {
-        connectedElement.style.display = 'none'
-        unconnectedElement.style.display = 'flex'
-      }
-    }
-
-    const loginDialog = buildFormDialog(
-      'login',
-      'http://localhost:3000/user/login',
-      loginFields,
+    const passwordField = this.shadowRoot.querySelector('input[name="password"]')
+    const confirmPasswordField = this.shadowRoot.querySelector(
+      'input[name="password-confirm"]',
     )
-    const signupDialog = buildFormDialog(
-      'signup',
-      'http://localhost:3000/user/register',
-      signupFields,
-    )
-    const passwordField = signupDialog
-      .querySelector('#signup-password')
-      .querySelector('input')
-    const confirmPasswordField = signupDialog
-      .querySelector('#signup-password-confirm')
-      .querySelector('input')
     const validatePassword = () => {
       if (passwordField.value !== confirmPasswordField.value) {
         confirmPasswordField.setCustomValidity('Passwords must match')
@@ -162,51 +126,71 @@ class PepeHeader extends HTMLElement {
     passwordField.addEventListener('change', validatePassword)
     confirmPasswordField.addEventListener('keyup', validatePassword)
 
-    const logoutButton = document.createElement('button')
-    logoutButton.textContent = 'logout'
+    const loginDialog = this.shadowRoot.querySelector('#login-dialog')
+    const signupDialog = this.shadowRoot.querySelector('#signup-dialog')
+
+    const loginButton = this.shadowRoot.querySelector('#login')
+    loginButton.addEventListener('click', () => loginDialog.showModal())
+    const signupButton = this.shadowRoot.querySelector('#signup')
+    signupButton.addEventListener('click', () => signupDialog.showModal())
+    const logoutButton = this.shadowRoot.querySelector('#logout')
     logoutButton.addEventListener('click', async () => {
-      try {
-        const response = await fetch('http://localhost:3000/user/logout', {
-          method: 'POST',
-          credentials: 'include',
-        })
-
-        if (response.ok) {
-          const message = await response.json()
-          alert(`Success: ${JSON.stringify(message)}`) //TEMP
-        } else {
-          const { message, error } = await response.json()
-          const errorMessage = message || error || JSON.stringify(response)
-          alert(`Error: ${errorMessage}`)
-        }
-      } catch (error) {
-        alert(`Error: ${error}`)
-      }
-
+      await this.logout()
       logoutButton.dispatchEvent(toggleConnectedEvent())
     })
 
-    const loginButton = document.createElement('button')
-    loginButton.textContent = 'login'
-    loginButton.addEventListener('click', () => loginDialog.showModal())
+    loginDialog
+      .querySelector('button[type="submit"]')
+      .addEventListener('click', dialogSubmit.bind(loginDialog))
+    loginDialog
+      .querySelector('button[type="reset"]')
+      .addEventListener('click', () => loginDialog.close())
+    signupDialog
+      .querySelector('button[type="submit"]')
+      .addEventListener('click', dialogSubmit.bind(signupDialog))
+    signupDialog
+      .querySelector('button[type="reset"]')
+      .addEventListener('click', () => signupDialog.close())
 
-    const signupButton = document.createElement('button')
-    signupButton.textContent = 'signup'
-    signupButton.addEventListener('click', () => signupDialog.showModal())
-
-    connectedElement.append(logoutButton)
-    unconnectedElement.append(loginButton, signupButton)
-    window.addEventListener('toggle-connected', toggleConnected)
-    this.shadowRoot.dispatchEvent(toggleConnectedEvent())
-
-    this.shadowRoot.append(
-      style,
-      home,
-      loginDialog,
-      signupDialog,
-      connectedElement,
-      unconnectedElement,
+    window.addEventListener(
+      'toggle-connected',
+      this._onToggleConnected.bind(this)
     )
+    this.shadowRoot.dispatchEvent(toggleConnectedEvent())
+  }
+
+  _onToggleConnected({ detail: { connected } }) {
+    const unconnectedElement = this.shadowRoot.getElementById('unconnected')
+    const connectedElement = this.shadowRoot.getElementById('connected')
+
+    //TODO: use css to hide/show (with properties)
+    if (connected) {
+      unconnectedElement.style.display = 'none'
+      connectedElement.style.display = 'flex'
+    } else {
+      connectedElement.style.display = 'none'
+      unconnectedElement.style.display = 'flex'
+    }
+  }
+
+  async logout() {
+    try {
+      const response = await fetch('http://localhost:3000/user/logout', {
+        method: 'POST',
+        credentials: 'include',
+      })
+
+      if (response.ok) {
+        const message = await response.json()
+        alert(`Success: ${JSON.stringify(message)}`) //TEMP
+      } else {
+        const { message, error } = await response.json()
+        const errorMessage = message || error || JSON.stringify(response)
+        alert(`Error: ${errorMessage}`)
+      }
+    } catch (error) {
+      alert(`Error: ${error}`)
+    }
   }
 }
 

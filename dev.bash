35d61e8770af5e6150c0deb8f1a99089d1a6bc5a
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-05-25 11:50:49 +0200

    feat: put every dev setup command in dev.bash
    damn this is easier

--- dev.bash
+++ dev.bash
@@ -1,2 +1,11 @@
+# build and run docker compose
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker compose build
 docker compose up -d
+
+# build api (TODO: remove this when api is back in compose file)
+cd api
+./dev.bash &
+cd ..
+
+# link pepe pictures
+./link_pepes.bash

d8add0d2a67d9d4ca8e06c97ff209f4f28e6c3f0
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-05-25 16:01:07 +0200

    fix: cleanup when stopping the app

--- dev.bash
+++ dev.bash
@@ -2,6 +2,9 @@
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker compose build
 docker compose up -d
 
+# wait for db to be ready
+sleep 7
+
 # build api (TODO: remove this when api is back in compose file)
 cd api
 ./dev.bash &
@@ -9,3 +12,22 @@ cd ..
 
 # link pepe pictures
 ./link_pepes.bash
+
+# test api
+#./test_api.bash
+
+# replace default password_hash
+#PASSWORD_HASH=$(
+#	docker exec camagru-db-1 psql -U postgres postgres \
+#	-c \
+#	"SELECT password_hash FROM accounts WHERE password_hash IS NOT 'toto' LIMIT 1;"
+#)
+#
+#echo "PASSWORD_HASH: $PASSWORD_HASH"
+
+# wait for input
+read
+
+# stop application
+kill -9 $(pidof target/debug/api)
+docker compose down

f50770c99990a60c68391f5459ba45a295334436
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-06-06 16:15:46 +0200

    feat: import picture and upload it with a pepe
    It WORKS !!!!!

--- front/src/pepe-upload.js
+++ front/src/pepe-upload.js
@@ -10,11 +10,13 @@ PepeUploadTemplate.innerHTML = `
   <link rel="stylesheet" href="style/global.css">
   <link rel="stylesheet" href="style/pepe-upload.css">
   <div id="pepe-upload">
+    <!--
     <div id="pepe-upload-capture" hidden>
       <video id="pepe-upload-capture-video" autoplay></video>
     </div>
+    -->
     <div id="pepe-upload-preview" hidden>
-      <canvas id="pepe-upload-preview-canvas"></canvas>
+      <img id="pepe-upload-preview-img" alt="Preview">
     </div>
     <div id="pepe-upload-toolbar">
       <select id="pepe-upload-toolbar-select">
@@ -22,9 +24,11 @@ PepeUploadTemplate.innerHTML = `
       </select>
       <label id="pepe-import-button" class="button" disabled="">
         Import Picture
-        <input type="file" disabled>
+        <input type="file" accept="image/*" capture="environment" disabled>
       </label>
+      <!--
       <button id="pepe-capture-button" disabled>Capture</button>
+      -->
       <button id="pepe-upload-button" disabled>Upload</button>
       <button id="pepe-cancel-button" disabled>Cancel</button>
     </div>
@@ -37,14 +41,18 @@ class PepeUpload extends HTMLElement {
     return ['data-superposable']
   }
 
-  constructor () {
+  constructor() {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(PepeUploadTemplate.content.cloneNode(true))
     this.setAttribute('data-superposable', '')
+
+    this.importInput.addEventListener('change', () => this.onImportInputChange())
+    this.uploadButton.addEventListener('click', () => this.upload())
+    this.cancelButton.addEventListener('click', () => this.cancel())
   }
 
-  async getSuperposables () {
+  async getSuperposables() {
     try {
       const url = `http://${window.location.hostname}:3000/pictures/superposable`
       const response = await fetch(url)
@@ -69,11 +77,11 @@ class PepeUpload extends HTMLElement {
     }
   }
 
-  attributeChangedCallback (name, oldValue, newValue) {
+  attributeChangedCallback(name, oldValue, newValue) {
     switch (name) {
       case 'data-superposable':
         this.disableImportButton(!newValue)
-        this.captureButton.disabled = !newValue
+        //this.captureButton.disabled = !newValue
         if (!newValue) {
           this.uploadButton.disabled = true
           this.cancelButton.disabled = true
@@ -91,31 +99,80 @@ class PepeUpload extends HTMLElement {
     this.importInput.disabled = disabled
   }
 
-  get superposable () {
+  get superposable() {
     return this.getAttribute('data-superposable')
   }
 
-  get importButton () {
+  get importButton() {
     return this.shadowRoot.querySelector('#pepe-import-button')
   }
 
-  get importInput () {
+  get importInput() {
     return this.shadowRoot.querySelector('#pepe-import-button input')
   }
 
-  get captureButton () {
-    return this.shadowRoot.querySelector('#pepe-capture-button')
+  get picture() {
+    return this.importInput.files[0]
   }
 
-  get uploadButton () {
+  /*
+  get captureButton() {
+    return this.shadowRoot.querySelector('#pepe-capture-button')
+  }
+  */
+
+  get uploadButton() {
     return this.shadowRoot.querySelector('#pepe-upload-button')
   }
 
-  get cancelButton () {
+  get cancelButton() {
     return this.shadowRoot.querySelector('#pepe-cancel-button')
   }
 
-  connectedCallback () {
+  get preview() {
+    return this.shadowRoot.querySelector('#pepe-upload-preview')
+  }
+
+  onImportInputChange() {
+    if (this.picture) {
+      this.preview.hidden = false
+      const img = this.preview.querySelector('img')
+      img.src = URL.createObjectURL(this.picture)
+      this.uploadButton.disabled = false
+      this.cancelButton.disabled = false
+    } else {
+      this.preview.hidden = true
+      this.uploadButton.disabled = true
+      this.cancelButton.disabled = true
+      }
+  }
+
+  cancel() {
+    this.importInput.value = ''
+    this.onImportInputChange()
+  }
+
+  async upload() {
+    try {
+      const { hostname } = window.location
+      const url = `http://${hostname}:3000/picture/${this.superposable}`
+      await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'image/jpeg' },
+        credentials: 'include',
+        body: this.picture,
+      })
+
+      //TODO: check that the response is OK or show an error
+      //then reset the upload form and hide it
+      //and show the new picture in the gallery
+    } catch (error) {
+      //TODO: remove this to "respect" the subject
+      console.error(error)
+    }
+  }
+
+  connectedCallback() {
     this.getSuperposables()
     const select = this.shadowRoot.querySelector('#pepe-upload-toolbar-select')
     select.addEventListener('input', (event) => {

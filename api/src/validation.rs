75168b609517cd0377085eb457d3388945f62d30
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-06-20 16:54:07 +0200

    refactor: use lazy_static to load regexes
    Optimimized

--- api/src/validation.rs
+++ api/src/validation.rs
@@ -1,15 +1,16 @@
 //! Api parameters validation.
 
+use lazy_static::lazy_static;
 use regex::{Regex, RegexSet};
 
 // Username is simply a string of six to sixty-four word characters.
-const USERNAME_REGEX: &str = r"^\w{6,64}$";
+const USERNAME_REGEX_STRING: &str = r"^\w{6,64}$";
 
 const PASSWORD_REGEX_COUNT: usize = 5;
 
 // Password must contain one lowercase letter, one uppercase letter, a digit, a
 // special character and must be at least 8 characters long.
-const PASSWORD_REGEX: [&str; PASSWORD_REGEX_COUNT] =
+const PASSWORD_REGEX_STRINGS: [&str; PASSWORD_REGEX_COUNT] =
     [r"[a-z]+", r"[A-Z]+", r"\d+", r"\W+", r".{8,}"];
 
 const PASSWORD_REGEX_ERRORS: [&str; PASSWORD_REGEX_COUNT] = [
@@ -23,15 +24,20 @@ const PASSWORD_REGEX_ERRORS: [&str; PASSWORD_REGEX_COUNT] = [
 // HTML5 email regex. The email addres must only contain alphanumeric and non
 // whitespace special characters for the first part. An '@' symbol and a domain
 // name with a least a '.' in it.
-const EMAIL_REGEX: &str =
+const EMAIL_REGEX_STRING: &str =
     r"^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
 
-//TODO: maybe use lazy_static macro crate to optimize the regex
+lazy_static! {
+    static ref USERNAME_REGEX: Regex =
+        Regex::new(USERNAME_REGEX_STRING).unwrap();
+    static ref PASSWORD_REGEX: RegexSet =
+        RegexSet::new(&PASSWORD_REGEX_STRINGS).unwrap();
+    static ref EMAIL_REGEX: Regex = Regex::new(EMAIL_REGEX_STRING).unwrap();
+}
 
 /// Check that the given username is a valid string
 pub fn username(username: &str) -> Result<(), String> {
-    let re = Regex::new(USERNAME_REGEX).unwrap();
-    if !re.is_match(username) {
+    if !USERNAME_REGEX.is_match(username) {
         return Err(String::from(
             "username must be a word of 6 to 64 characters long",
         ));
@@ -41,8 +47,8 @@ pub fn username(username: &str) -> Result<(), String> {
 
 /// Check that the password is long and strong enough
 pub fn password(password: &str) -> Result<(), String> {
-    let set = RegexSet::new(&PASSWORD_REGEX).unwrap();
-    let matches: Vec<_> = set.matches(password).into_iter().collect();
+    let matches: Vec<_> =
+        PASSWORD_REGEX.matches(password).into_iter().collect();
     if matches.len() != PASSWORD_REGEX_COUNT {
         for (index, error) in PASSWORD_REGEX_ERRORS.iter().enumerate() {
             if !matches.contains(&index) {
@@ -55,8 +61,7 @@ pub fn password(password: &str) -> Result<(), String> {
 
 /// Check that the email actually looks like an email
 pub fn email(email: &str) -> Result<(), String> {
-    let re = Regex::new(EMAIL_REGEX).unwrap();
-    if !re.is_match(email) || email.len() > 256 {
+    if !EMAIL_REGEX.is_match(email) || email.len() > 256 {
         return Err(String::from("invalid email format"));
     }
     Ok(())

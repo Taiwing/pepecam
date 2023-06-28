45d5f486a32f8018a1a6aa60e591a4808daf4076
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-06-28 12:13:29 +0200

    feat: make it possible to use ctrl+C to quit

--- scripts/dev.bash
+++ scripts/dev.bash
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+# stop application and exit
+function stop() {
+    docker compose down
+    exit 0
+}
+
 # go to repo root
 cd $(git rev-parse --show-toplevel)
 
 # build and run docker compose
 docker compose up --build --wait
+
+# set SIGINT handler
+trap 'stop' SIGINT
 
 # link pepe pictures
 ./scripts/link_pepes.bash
@@ -13,4 +22,4 @@ docker compose up --build --wait
 read
 
 # stop application
-docker compose down
+stop

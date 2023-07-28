72fbd867983f6c5400a003fd770fc1308e7a5b33
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-07-28 12:43:44 +0200

    feat: set copyright and license for pepecam

--- front/src/pepe-footer.js
+++ front/src/pepe-footer.js
@@ -6,14 +6,41 @@ PepeFooterTemplate.innerHTML = `
     <a href="/editor.html">Editor</a>
     <a href="/profile.html">Profile</a>
   </nav>
+  <p class="footer-text">
+    ©
+    <span id="years"></span>
+    <span id="author"></span>
+    --
+    <a id="license"></a>
+  </p>
 `
 
 // PepeFooter element
 class PepeFooter extends HTMLElement {
+  author = 'Yoann FOREAU'
+  license = 'GNU General Public License v3'
+  licenseUrl = 'https://www.gnu.org/licenses/gpl-3.0.html#license-text'
+  startYear = 2022
+
+  get currentYear() {
+    return new Date().getFullYear()
+  }
+
+  get years() {
+    return this.startYear === this.currentYear
+      ? this.startYear
+      : `${this.startYear}-${this.currentYear}`
+  }
+
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.append(PepeFooterTemplate.content.cloneNode(true))
+
+    this.shadowRoot.getElementById('years').innerText = this.years
+    this.shadowRoot.getElementById('author').innerText = this.author
+    this.shadowRoot.getElementById('license').innerText = this.license
+    this.shadowRoot.getElementById('license').href = this.licenseUrl
   }
 }
 
